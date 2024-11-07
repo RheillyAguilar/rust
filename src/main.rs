9ed755464f1308aa176bbe9d6d8f8d9a690e45a3
Rheilly Aguilar <rheilly17@gmail.com>
2024-11-07 14:22:43 +0800

    add slice

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,26 @@
 
 fn main() {
 
-    // let s: &str = "hello, world";
+    // let arr: [i32; 3] = [1, 2, 3];
+    // let s1: &[i32] = &arr[0..2];
+    // let s2: &str = "hello, world";
 
-    // let s: &str = "hello, world";
-    // greetings(s)
-    // }
-    // fn greetings(s: &str) {
-    //     println!("{}",s)
-    // }
+    // let arr: [char; 3] = ['中', '国', '人'];
+    // let slice: &[char] = &arr[..2];
+    // assert!(std::mem::size_of_val(&slice) == 16);
 
-    // let mut s: String = String::from("");
-    // s.push_str("hello, world");
-    // s.push('!');
-    // assert_eq!(s, "hello, world!");
+    // let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    // let slice: &[i32] = &arr[1..4];
+    // assert_eq!(slice, &[2, 3, 4]);
 
-    // let mut s: String = String::from("hello");
-    // s.push(',');
-    // s.push_str(" world");
-    // s += "!";
-    // println!("{}", s);
+    // let s: String = String::from("hello");
+    // let slice1: &str = &s[0..2];
+    // let slice2: &str = &s[..2];
+    // assert_eq!(slice1, slice2);
 
-    // let mut s: String = String::from("I like dogs");
-    // let s1 = s.replace("dogs", "cats");
-    // assert_eq!(s1, "I like cats");
-
-    // let s1: String = String::from("hello,");
-    // let s2: String = String::from("world!");
-    // let s3: String = s1 + s2.as_str(); 
-    // assert_eq!(s3, "hello,world!");
-    // println!("{}", s3);
-
-    // let s: &str = "hello, world";
-    // greetings(s.to_string());
-    // }
-    // fn greetings(s: String) {
-    //     println!("{}", s)
-    // }
-
-    // let s: String = "hello, world".to_string();
-    // let s1: &str = s.as_str();
-
+    // let s: &str = "你好，世界";
+    // let slice: &str = &s[0..3];
+    // assert!(slice == "你");
 
 
     // println!("Success!");
