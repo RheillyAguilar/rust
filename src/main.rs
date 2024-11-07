2d0243422e3a39616527c66dde8a4085293d7134
Rheilly Aguilar <rheilly17@gmail.com>
2024-11-07 09:30:40 +0800

    char bool unit

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 
-use std::ops::{Range, RangeInclusive};
+// use std::mem::size_of_val;
 
 fn main() {
 
-    
-    // let mut sum = 0;
-    // for i in -3..2 {
-    //     sum += i
-    // }
-    // assert!(sum == -5);
-    // for c in 'a'..='z' {
-    //     println!("{}",c as u8);
-    // }
-    // assert_eq!((1..5), Range{ start: 1, end: 5 });
-    // assert_eq!((1..=5), RangeInclusive::new(1, 5));
-
-    //  // Integer addition
-    //  assert!(1u32 + 2u32 == 3u32);
-    //  // Integer subtraction
-    //  assert!(1i32 - 2i32 == -1i32);
-    //  assert!(1i8 - 2i8 == -1);
-    //  assert!(3 * 50 == 150);
-    //  assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32); // error ! make it work
-    //  assert!(24 % 5 == 4);
-    //  // Short-circuiting boolean logic
-    //  assert!(true && false == false);
-    //  assert!(true || false == true);
-    //  assert!(!true == false);
-    //  // Bitwise operations
-    //  println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    //  println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    //  println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    //  println!("1 << 5 is {}", 1u32 << 5);
-    //  println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
     println!("Success!");
 
-
-}
+} 
 
