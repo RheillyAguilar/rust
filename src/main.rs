b7d417fa141694450f2abee0260c037b1f95d00e
Rheilly Aguilar <rheilly17@gmail.com>
2024-11-07 14:31:45 +0800

    add tuple

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,27 @@
 
 fn main() {
 
-    // let arr: [i32; 3] = [1, 2, 3];
-    // let s1: &[i32] = &arr[0..2];
-    // let s2: &str = "hello, world";
+    // let _t0: (u8,i16) = (0, -1);
+    // let _t1: (u8, (i16, u32)) = (0, (-1, 1));
+    // let t: (u8, u16, i64, &str, String) = (1u8, 2u16, 3i64, "hello", String::from(", world"));
 
-    // let arr: [char; 3] = ['中', '国', '人'];
-    // let slice: &[char] = &arr[..2];
-    // assert!(std::mem::size_of_val(&slice) == 16);
+    // let t: (&str, &str, &str) = ("i", "am", "sunface");
+    // assert_eq!(t.2, "sunface");
 
-    // let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    // let slice: &[i32] = &arr[1..4];
-    // assert_eq!(slice, &[2, 3, 4]);
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    // println!("too long tuple: {:?}", too_long_tuple);
 
-    // let s: String = String::from("hello");
-    // let slice1: &str = &s[0..2];
-    // let slice2: &str = &s[..2];
-    // assert_eq!(slice1, slice2);
+    // let tup: (i32, f64, &str) = (1, 6.4, "hello");
+    // let (x,z,y) = tup;
+    // assert_eq!(x, 1);
+    // assert_eq!(y, "hello");
+    // assert_eq!(z, 6.4);
 
-    // let s: &str = "你好，世界";
-    // let slice: &str = &s[0..3];
-    // assert!(slice == "你");
+    // let (x, y, z);
+    // (y, z, x) = (1, 2, 3);
+    // assert_eq!(x, 3);
+    // assert_eq!(y, 1);
+    // assert_eq!(z, 2);
 
 
     // println!("Success!");
