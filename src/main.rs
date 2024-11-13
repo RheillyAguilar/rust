62bea1f20c8d6c8a409c51b0dd29142610d814b8
Rheilly Aguilar <rheilly17@gmail.com>
2024-11-13 08:46:54 +0800

    add struct

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,7 @@
 
 fn main() {
 
-    // let _t0: (u8,i16) = (0, -1);
-    // let _t1: (u8, (i16, u32)) = (0, (-1, 1));
-    // let t: (u8, u16, i64, &str, String) = (1u8, 2u16, 3i64, "hello", String::from(", world"));
 
-    // let t: (&str, &str, &str) = ("i", "am", "sunface");
-    // assert_eq!(t.2, "sunface");
-
-    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-    // println!("too long tuple: {:?}", too_long_tuple);
-
-    // let tup: (i32, f64, &str) = (1, 6.4, "hello");
-    // let (x,z,y) = tup;
-    // assert_eq!(x, 1);
-    // assert_eq!(y, "hello");
-    // assert_eq!(z, 6.4);
-
-    // let (x, y, z);
-    // (y, z, x) = (1, 2, 3);
-    // assert_eq!(x, 3);
-    // assert_eq!(y, 1);
-    // assert_eq!(z, 2);
 
 
     // println!("Success!");
